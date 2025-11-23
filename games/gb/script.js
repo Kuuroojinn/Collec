15f00a74edae8f946b46bbad720a08b6abc2487a
Hugo Martin <hugomartinsi2020@gmail.com>
2025-11-23 16:24:29 +0100

    corrected gb page

--- games/gb/script.js
+++ games/gb/script.js
@@ -16,7 +16,7 @@ function generateTable(data) {
   table.appendChild(headerRow);
   // Generate table rows
   data.forEach(item => {
-	  if (item["Platform"]=="GB" || item["Platform"}=="GBC") {
+	  if (item["Platform"]=="GB" || item["Platform"]=="GBC") {
     const row = document.createElement('tr');
     keys.forEach(key => {
       const td = document.createElement('td');
