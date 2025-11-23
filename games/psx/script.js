75f2034d2caff231e87e852784a399768d68cdd9
Hugo Martin <hugomartinsi2020@gmail.com>
2025-11-23 16:28:30 +0100

    corrected few pages

--- games/psx/script.js
+++ games/psx/script.js
@@ -16,7 +16,7 @@ function generateTable(data) {
   table.appendChild(headerRow);
   // Generate table rows
   data.forEach(item => {
-	  if (item["Platform"]=="PS3") {
+	  if (item["Platform"]=="PSX") {
     const row = document.createElement('tr');
     keys.forEach(key => {
       const td = document.createElement('td');
